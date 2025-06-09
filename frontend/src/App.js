a566146ce40e2267db3b5e9002e487e9a07aa341
Vishnu Mehta <vishnu.mehta@tum.de>
2025-06-09 20:13:02 +0200

    Add: Room Details page with booking form and mock data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,27 @@ import SignUpSide from "./SignUpSide";
 import Homepage from './Homepage';
 import Floorplan from './floorplan';
 
+import RoomDetails from './RoomDetails.jsx';
+
+<Route path="/room-details" element={<RoomDetails />} />
+
+
 function App() {
     return (
         <Router>
             <div className="App">
-                <Routes>
-                    <Route path="/" element={<SignInSide />} />
-                    <Route path="/signup" element={<SignUpSide />} />
-                    <Route path="/homepage" element={<Homepage />} />
-                    <Route path="/floor-plan" element={<Floorplan/>} />
-                    <Route path="*" element={<h2>Page Not Found</h2>} />
-                </Routes>
+               <Routes>
+  <Route path="/" element={<SignInSide />} />
+  <Route path="/signup" element={<SignUpSide />} />
+  <Route path="/homepage" element={<Homepage />} />
+  <Route path="/floor-plan" element={<Floorplan/>} />
+  <Route path="/room-details" element={<RoomDetails />} />
+  <Route path="*" element={<h2>Page Not Found</h2>} />
+</Routes>
+
             </div>
         </Router>
     );
 }
 
-export default App;
+export default App; 
