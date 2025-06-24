6477c59453a8f719e27be03a6dd317e4538704f4
Thanawan Panapongpaisan <suithanawan@gmail.com>
2025-06-24 10:37:43 +0200

    update floorplan

--- backend/models/floor.js
+++ backend/models/floor.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const floorSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    building: { type: String, required: true },
     image: { type: String, required: true },
 });
 
